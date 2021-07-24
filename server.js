53864459dccb15765b8614f66e820880cacdbfd0
Tracy Angell <63623364+tracye1083@users.noreply.github.com>
2021-07-23 22:27:57 -0400

    created file pathing
    copied from mini project in week 18 class work

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = 3000;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/budget';
 
 const app = express();
 
@@ -15,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
